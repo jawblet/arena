5227975ef505cd3a275dc735a278df720de1576f
jawblia <juliabell021@gmail.com>
2021-04-09 13:05:19 -0400

    phrases from shea's dreams

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Paginate from './components/Paginate';
+import Card from './components/Card';
 
 function App() {
+  const postsPerPage = 10;
+  const [phrases, setPhrases] = useState(null);
+  const [total, setTotal] = useState(1);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    axios.get('https://api.are.na/v2/channels/shea-phrases-from-my-dreams', 
+    { params: {
+      per: postsPerPage,
+      page: page}
+    }).then(res => {
+      setPhrases(res.data.contents);
+      setTotal(Math.ceil(res.data.length / postsPerPage));
+    }).catch(err => console.log(err));
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+        <h3>phrases from shea's dreams</h3>
+        <div className="page__counter">
+          <Paginate page={page} total={total} setPage={setPage}/>
+          </div>
+        <div className="page__content">
+          {phrases && phrases.map((el, i) => {
+            return (<Card key={i} text={el.content}/>)
+          })}
+        </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Card = ({ text }) => {
+
+    return (<div className="card">
+        <p>{text}</p>
+    </div>);
+}
+ 
+export default Card;
